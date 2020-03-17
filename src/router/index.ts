90b760b9a3803971fb5189e1fda2ebd9fb71081b
CNMathon <wsnbtkk@163.com>
2020-03-17 17:07:19 +0800

    fix: fix white screen of homepage content caused by root page not automatically jumping

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -15,7 +14,7 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    redirect: '/wallet'
   },
   {
     path: 'About',
