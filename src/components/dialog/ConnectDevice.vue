bd50b3c44e0bb17150bdd3fde70495d4658bcbab
CNMathon <wsnbtkk@163.com>
2020-03-20 19:17:16 +0800

    fix(i18n): fix i18n configuration error

--- src/components/dialog/ConnectDevice.vue
+++ src/components/dialog/ConnectDevice.vue
@@ -10,7 +10,7 @@
         <v-col cols="12">
           <v-btn dark rounded color="primary" @click="m_connect">
             <v-icon dark>mdi-plus</v-icon>
-            <span class="subtitle-2 pl-2">{{ $t('Waiting for device to connect') }}</span>
+            <span class="subtitle-2 pl-2">{{ $t('Check For Device') }}</span>
           </v-btn>
         </v-col>
       </v-row>
