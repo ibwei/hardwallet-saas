9b24caf9cf4074f457ea5dc9c2fe0ed0cc4ff42e
CNMathon <wsnbtkk@163.com>
2020-03-26 17:44:49 +0800

    fix: fixed the bug of device's label name of TopBar

--- src/views/components/TopBar.vue
+++ src/views/components/TopBar.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="d-flex flex-row justify-end align-center blue lighten-3 shadow">
-    <div class="d-flex flex-row justify-center align-center pa-2 ma-2 pl-4 pr-4 blue lighten-5" style="border-radius:20px; width:auto;">
+    <div
+      class="d-flex flex-row justify-center align-center pa-2 ma-2 pl-4 pr-4 blue lighten-5"
+      style="border-radius:20px; width:auto;"
+    >
       <div class="body-2">{{ c_deviceName }}</div>
       <i class="icon pl-2">&#xe606;</i>
     </div>
@@ -8,12 +11,11 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
   computed: {
-    ...mapState(['usb']),
     c_deviceName() {
-      return this.usb.connect ? this.usb.product : 'Waiting for connect'
+      if (this.$store.__s('usb.connect')) return this.$store.__s('usb.label') ? this.$store.__s('usb.label') : this.$store.__s('usb.product')
+      else return 'Waiting for connect'
     }
   }
 }
